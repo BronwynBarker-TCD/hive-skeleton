e5cab5c9956ca4fc96ec72acab9f67275d6c31f0
Tanzelle-tcd <46919835+Tanzelle-tcd@users.noreply.github.com>
2019-02-20 07:34:07 +0200

    Add files via upload
    Hive prototype files

---
+++ 1 Hive skeleton/app/__init__.py
@@ -0,0 +1,55 @@
+import logging
+from flask import Flask, redirect
+from flask_appbuilder import AppBuilder, IndexView, SQLA
+from flask_appbuilder.baseviews import expose
+
+"""
+ Logging configuration
+"""
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLA(app)
+#appbuilder = AppBuilder(app, db.session) #See MODIFICATIONS
+
+
+"""
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+#Only include this for SQLLite constraints
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    # Will force sqllite contraint foreign keys
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+"""
+
+#MODIFICAIONS
+
+#Own index page
+class MyIndexView(IndexView):
+    index_template = 'welcome.html'
+
+appbuilder = AppBuilder(
+    app,
+    db.session,
+    indexview=MyIndexView,
+)
+
+
+#IMPORT ADDDITIONAL MODELS HERE!#
+#from app.models import models
+
+
+#IMPORT ADDDITIONAL VIEWS HERE!#
+#from app.views import views
+
+
+#IMPORT ADDITIONAL MODULES HERE!#
+from app.upload.models import files
+from app.upload.views import files
---
+++ 1 Hive skeleton/app/upload/views/files.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 13:55:36 2019
+
+@author: Tanzelle.Oberholster
+"""
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder.views import ModelView, CompactCRUDMixin
+from app.upload.models.files import Project, ProjectFiles
+from app import appbuilder, db
+
+class ProjectFilesModelView(ModelView):
+    datamodel = SQLAInterface(ProjectFiles)
+
+    label_columns = {'file_name': 'File Name', 'download': 'Download'}
+    add_columns = ['file', 'description']
+    edit_columns = ['file', 'description']
+    list_columns = ['file_name', 'download']
+    show_columns = ['file_name', 'download']
+
+
+class ProjectModelView(CompactCRUDMixin, ModelView):
+    datamodel = SQLAInterface(Project)
+    related_views = [ProjectFilesModelView]
+
+    show_template = 'appbuilder/general/model/show_cascade.html'
+    edit_template = 'appbuilder/general/model/edit_cascade.html'
+
+    add_columns = ['name']
+    edit_columns = ['name']
+    list_columns = ['name', 'created_by', 'created_on', 'changed_by', 'changed_on']
+    show_fieldsets = [
+        ('Info', {'fields': ['name']}),
+        ('Audit', {'fields': ['created_by', 'created_on', 'changed_by', 'changed_on'], 'expanded': False})
+    ]
+
+
+db.create_all()
+
+appbuilder.add_view(ProjectModelView, "List Folders", icon="fa-table", category="Uploads")
+appbuilder.add_view(ProjectFilesModelView, "List Files", icon="fa-table", category="Uploads")
